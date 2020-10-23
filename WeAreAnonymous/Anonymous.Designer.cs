97bf510e4abffd5c551baf7cc62e497f3dc450c1
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2020-10-23 22:17:07 +1300

    Add files via upload

---
+++ WeAreAnonymous/Anonymous.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WeAreAnonymous
+{
+    partial class Anonymous
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anonymous));
+            this.CreateVideo = new System.Windows.Forms.Button();
+            this.TestTTS = new System.Windows.Forms.Button();
+            this.TTS = new System.Windows.Forms.RichTextBox();
+            this.CurrentProcess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreateVideo
+            // 
+            this.CreateVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CreateVideo.BackColor = System.Drawing.Color.Black;
+            this.CreateVideo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateVideo.Location = new System.Drawing.Point(12, 609);
+            this.CreateVideo.Name = "CreateVideo";
+            this.CreateVideo.Size = new System.Drawing.Size(388, 23);
+            this.CreateVideo.TabIndex = 1;
+            this.CreateVideo.Text = "EXPECT US";
+            this.CreateVideo.UseVisualStyleBackColor = false;
+            this.CreateVideo.Click += new System.EventHandler(this.CreateVideo_Click);
+            // 
+            // TestTTS
+            // 
+            this.TestTTS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.TestTTS.BackColor = System.Drawing.Color.Black;
+            this.TestTTS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TestTTS.Location = new System.Drawing.Point(415, 609);
+            this.TestTTS.Name = "TestTTS";
+            this.TestTTS.Size = new System.Drawing.Size(401, 23);
+            this.TestTTS.TabIndex = 2;
+            this.TestTTS.Text = "WE ARE LEGION";
+            this.TestTTS.UseVisualStyleBackColor = false;
+            this.TestTTS.Click += new System.EventHandler(this.TestTTS_Click);
+            // 
+            // TTS
+            // 
+            this.TTS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TTS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.TTS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TTS.DetectUrls = false;
+            this.TTS.ForeColor = System.Drawing.Color.Lime;
+            this.TTS.Location = new System.Drawing.Point(12, 12);
+            this.TTS.Name = "TTS";
+            this.TTS.Size = new System.Drawing.Size(804, 570);
+            this.TTS.TabIndex = 3;
+            this.TTS.Text = resources.GetString("TTS.Text");
+            // 
+            // CurrentProcess
+            // 
+            this.CurrentProcess.AutoSize = true;
+            this.CurrentProcess.Location = new System.Drawing.Point(12, 585);
+            this.CurrentProcess.Name = "CurrentProcess";
+            this.CurrentProcess.Size = new System.Drawing.Size(302, 13);
+            this.CurrentProcess.TabIndex = 4;
+            this.CurrentProcess.Text = "WE DO NOT FORGIVE, WE DO NOT FORGET, EXPECT US";
+            // 
+            // Anonymous
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(16)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(828, 644);
+            this.Controls.Add(this.CurrentProcess);
+            this.Controls.Add(this.TTS);
+            this.Controls.Add(this.TestTTS);
+            this.Controls.Add(this.CreateVideo);
+            this.ForeColor = System.Drawing.Color.SpringGreen;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Anonymous";
+            this.Text = "We Are Anonymous";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Anonymous_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CreateVideo;
+        private System.Windows.Forms.Button TestTTS;
+        private System.Windows.Forms.RichTextBox TTS;
+        private System.Windows.Forms.Label CurrentProcess;
+    }
+}
+
---
+++ WeAreAnonymous/Anonymous.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Diagnostics;
+
+namespace WeAreAnonymous
+{
+    public partial class Anonymous : Form
+    {
+        public Anonymous()
+        {
+            InitializeComponent();
+        }
+
+        private void TestTTS_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer speechSynth = new SpeechSynthesizer();
+            ReadOnlyCollection<InstalledVoice> voices = speechSynth.GetInstalledVoices();
+            foreach(InstalledVoice voice in voices)
+            {
+                if(voice.VoiceInfo.Name.ToLower().Contains("david") || voice.VoiceInfo.Name.ToLower().Contains("sam"))
+                {
+                    speechSynth.SelectVoice(voice.VoiceInfo.Name);
+                    break;
+                }
+            }
+            Prompt prompt = new Prompt(TTS.Text);
+            speechSynth.SpeakAsync(prompt);
+        }
+
+        private void CreateVideo_Click(object sender, EventArgs e)
+        {
+            CreateVideo.Enabled = false;
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Application.ExecutablePath));
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "We Are Legion.mp4";
+            sfd.DefaultExt = "mp4";
+            sfd.Filter = "MPEG-4 Videos|*.mp4";
+            sfd.ShowDialog();
+
+            string outputPath = sfd.FileName;
+
+            SpeechSynthesizer speechSynth = new SpeechSynthesizer();
+            ReadOnlyCollection<InstalledVoice> voices = speechSynth.GetInstalledVoices();
+            foreach (InstalledVoice voice in voices)
+            {
+                if (voice.VoiceInfo.Name.ToLower().Contains("david") || voice.VoiceInfo.Name.ToLower().Contains("sam"))
+                {
+                    speechSynth.SelectVoice(voice.VoiceInfo.Name);
+                    break;
+                }
+            }
+
+            CurrentProcess.Text = "Generating TTS";
+            Prompt prompt = new Prompt(TTS.Text);
+            if (File.Exists("WeAreLegion.wav"))
+                File.Delete("WeAreLegion.wav");
+            FileStream wavStream = File.OpenWrite("WeAreLegion.wav");
+            speechSynth.SetOutputToWaveStream(wavStream);
+            speechSynth.SpeakAsync(prompt);
+            while (!prompt.IsCompleted)
+                Application.DoEvents();
+            wavStream.Dispose();
+
+            CurrentProcess.Text = "Generating Video...";
+
+            Process ffmpeg = new Process();
+            ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+            ffmpeg.StartInfo.Arguments = "-stream_loop -1 -i AnnonLoop.mp4 -i WeAreLegion.wav -shortest -map 0:v:0 -map 1:a:0 -y WeAreLegion.mp4";
+            ffmpeg.StartInfo.CreateNoWindow = true;
+            ffmpeg.StartInfo.UseShellExecute = false;
+            ffmpeg.Start();
+
+
+            while (!ffmpeg.HasExited)
+                Application.DoEvents();
+
+            if (File.Exists("WeAreLegion.wav"))
+                File.Delete("WeAreLegion.wav");
+
+
+            CurrentProcess.Text = "Adding Intro Video...";
+
+            ffmpeg = new Process();
+            ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+            ffmpeg.StartInfo.Arguments = "-i AnnonIntro.mp4 -i WeAreLegion.mp4 -filter_complex \"[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\" -y \"" + outputPath+"\"";
+            ffmpeg.StartInfo.CreateNoWindow = true;
+            ffmpeg.StartInfo.UseShellExecute = false;
+            ffmpeg.Start();
+
+            while (!ffmpeg.HasExited)
+                Application.DoEvents();
+
+            ffmpeg.Dispose();
+
+            if (File.Exists("WeAreLegion.mp4"))
+                File.Delete("WeAreLegion.mp4");
+
+            CurrentProcess.Text = "WE DO NOT FORGIVE, WE DO NOT FORGET, EXPECT US";
+
+            MessageBox.Show("YOU ARE ANONYMOUS", "WE ARE LEGION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            CreateVideo.Enabled = true;
+        }
+
+        private void Anonymous_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process[] procs = Process.GetProcessesByName("ffmpeg");
+            foreach(Process proc in procs)
+            {
+                proc.Kill();
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
